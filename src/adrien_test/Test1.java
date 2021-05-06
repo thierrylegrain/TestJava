46e64130fec8374cc5ab072a1cb5daede81f7283
Thierry Legrain <tlegrain@infovista.com>
2021-05-06 08:19:52 +0200

    adding camembert

--- src/adrien_test/Test1.java
+++ src/adrien_test/Test1.java
@@ -13,6 +13,9 @@ public class Test1 {
 			case 3:
 				System.out.println("Hello Adrien " + "olive");
 				break;
+			case 4:
+				System.out.println("Hello Adrien " + "camembert");
+				break;
 			default:
 				System.out.println("Hello Adrien " + i);
 				break;
