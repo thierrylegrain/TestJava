29f6279500ff87de28f31a842e519c43392061d8
Thierry Legrain <tlegrain@infovista.com>
2021-05-06 08:18:36 +0200

    Initial commit

---
+++ src/adrien_test/Test1.java
@@ -0,0 +1,23 @@
+package adrien_test;
+
+public class Test1 {
+	public static void main(String[] args) {
+		for (int i = 0; i < 10; i++) {
+			switch (i) {
+			case 1:
+				System.out.println("Hello Adrien " + "fromage");
+				break;
+			case 2:
+				System.out.println("Hello Adrien " + "saucisson");
+				break;
+			case 3:
+				System.out.println("Hello Adrien " + "olive");
+				break;
+			default:
+				System.out.println("Hello Adrien " + i);
+				break;
+			}
+		}
+		
+	}
+}
